2d2775d6cb79db66c614820a10665ab93051943d
Zwu <rewreu@Gmail.com>
2024-02-02 11:52:37 -0500

    Update set_env_variable.sh

--- shell/set_env_variable.sh
+++ shell/set_env_variable.sh
@@ -10,17 +10,20 @@ array_length=${#variables[@]}
 
 # Loop through the arrays by index
 for (( i=0; i<array_length; i++ )); do
-  # Check if the environment variable (by index) is set and not empty
-  if [ -z "${!env_vars[i]}" ]; then
-    # If not set, use the constant string
+  # Retrieve the current environment variable's value
+  current_env_var_value="${!env_vars[i]}"
+  
+  # Check if the environment variable is set to 'NA' or is empty
+  if [[ -z "$current_env_var_value" || "$current_env_var_value" == "NA" ]]; then
+    # If it is 'NA' or empty, use the constant string
     eval ${variables[i]}='${constants[i]}'
   else
-    # If set, use the environment variable's value
-    eval ${variables[i]}='${!env_vars[i]}'
+    # If it exists and is not 'NA' nor empty, use the environment variable's value
+    eval ${variables[i]}='${current_env_var_value}'
   fi
 done
 
 # Example to display the values
 for var in "${variables[@]}"; do
-  eval echo "\$var is set to: \$${var}"
+  eval echo "$var is set to: \$${var}"
 done
